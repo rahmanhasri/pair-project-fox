a6f4ac5b5dc8d1260cd6affe9ca4ccaca33db165
Celyn Susanto <celyn.vy@gmail.com>
2019-01-30 11:28:41 +0700

    add session and midware

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 const UserRoute = require('./routes/user')
+const session = require('express-session')
+const middleware = require('./helpers/middleware')
 
 app.set('view engine', 'ejs')
 app.set('views', './views/pages')
 app.use(express.urlencoded( { extended : false }))
 app.use(express.static('public'))
+app.use(session({
+  secret: 'flazz'
+}))
 
 app.get('/', function(req, res) {
   res.render('home')
---
+++ helpers/middleware.js
@@ -0,0 +1,7 @@
+module.exports = function(req, res, next) {
+    if (req.session.userLogin) {
+        next()
+    } else {
+        res.redirect('/user/login')
+    }
+}
