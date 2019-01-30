eb867bfe2a85ab5bd7fbd2aee8a895e6dfcc9a92
rahmanhasri <rahman.hasri@gmail.com>
2019-01-30 08:22:26 +0700

    pagi

--- routes/login.js
+++ routes/login.js
@@ -4,15 +4,17 @@ const User = Model.User
 const bcryptLogin = require('../helpers/login')
 
 router.get('/', function(req, res) {
-  res.render('login', { err : req.query.err })
+  res.render('./pages/login', { err : req.query.err })
 })
 
 router.post('/', function(req, res) {
   // res.send(req.body)
   User.findOne( { where : { username : req.body.username, }})
     .then( user => {
-      let cekLogin = bcryptLogin(user.password, req.body.password)
-      cekLogin ? res.send('Login Berhasil') : res.send('Login Gagal')
+      return bcryptLogin(user.password, req.body.password)
+    })
+    .then( login => {
+      login ? res.send('Login Berhasil') : res.send('Login Gagal')
     })
     .catch( err => {
       res.send(err)
